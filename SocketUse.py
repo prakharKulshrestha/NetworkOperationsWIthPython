e33f0e4562e311bc12f46c3c205bf719646f7f22
Prakhar Kulshrestha <prakhar.kulshrestha1801@gmail.com>
2019-02-18 21:09:36 -0500

    Use of socket to make connection

--- SocketUse.py
+++ SocketUse.py
@@ -2,8 +2,8 @@ import socket
 
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.connect(('wiki.python.org', 80))
+cmd = 'GET https://wiki.python.org/moin/ HTTP/1.0\r\n\r\n'.encode()
 handler=mysock.send(cmd)
 
 
